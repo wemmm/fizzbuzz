7f7abbbde78d73f85f1aa323800c511240986987
wemmm <jenny.wem@gmail.com>
2017-05-02 14:22:24 +0100

    Updated fizzbuzz.rb and fizzbuzz_spec to test for multiples of both 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(numbers)
- if numbers % 3 == 0
+  if numbers % 3 == 0 && numbers % 5 == 0
+    return 'fizzbuzz'
+  elsif numbers % 3 == 0
    return 'fizz'
  elsif numbers % 5 == 0
    return 'buzz'
-else
-  'fizzbuzz'
  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
 end
